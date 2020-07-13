0da076620f5d3a58c9536572ed091808bfc3f4b1
lyndsiWilliams <kcatgirl@gmail.com>
2020-07-13 15:55:46 -0500

    Completed stack.py

--- stack/stack.py
+++ stack/stack.py
@@ -9,17 +9,32 @@ return elements in Last In First Out order.
    Make sure the Stack tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
+   - In an array, each element is independent and can be accessed using it's index.
+     In a linked list, the element only has access to itself and the elements directly
+     in front of and/or behind it.
 """
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        # Return current size
+        return self.size
 
     def push(self, value):
-        pass
+        # Add value to the end of storage list
+        self.storage.append(value)
+        # Set size to storage length
+        self.size = len(self.storage)
 
     def pop(self):
-        pass
+        # Check if list is empty
+        if self.size == 0:
+            return None
+        # Assign popped value to popped variable
+        popped = self.storage.pop()
+        # Set size to storage list length
+        self.size = len(self.storage)
+        # Return popped value
+        return popped
